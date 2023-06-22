0d1d99fa5ceca141bdd7545a907bf561851be83f
Richard Breno <filip_2001_richard2022@hotmail.com>
2023-06-22 11:24:29 -0300

    commit

--- projeto-loja.js
+++ projeto-loja.js
@@ -1,6 +1,6 @@
 let hamb = document.getElementById('hamb')
 let navCelular = document.querySelector('#nav-celular')
-
+let NavDesktop = document.getElementById('nav-desktop')
 //Evento para a barra de navegação na versão de celular/tablet
 hamb.addEventListener('click',()=>{
     if(navCelular.style.display == 'none') {
@@ -12,8 +12,18 @@ hamb.addEventListener('click',()=>{
 function MonitorarLargura() {
     if(window.innerWidth > 1280) {
         hamb.style.display = 'none'
+        NavDesktop.style.display = 'block'
         navCelular.style.display = 'none'
     } else {
         hamb.style.display = 'block'
+        NavDesktop.style.display = 'none'
+    }
+}
+function Carregar() {
+    if(window.innerWidth > 1280) {
+        hamb.style.display = 'none'
+    }
+    if(window.innerWidth < 1280) {
+        NavDesktop.style.display = 'none'
     }
 }
